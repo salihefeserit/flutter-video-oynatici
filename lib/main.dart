226827e308fc0d8a57210bc9fceb0d510c60f539
Salih Efe Şerit <salihefeserit@gmail.com>
2025-11-29 00:15:48 +0300

    Playlistsiz Son Hali (fork edilerek playlist ekleme denemesi yapılabilir.)

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,18 @@ class _VideoScreenState extends State<VideoScreen> {
   bool _looping = true;
   bool _fullscreen = false;
 
+  List<Uri> videos = [
+    Uri.parse("https://video-previews.elements.envatousercontent.com/9376b5da-eeec-4497-81ec-24974097b70c/watermarked_preview/watermarked_preview.mp4"),
+    Uri.parse(" "),
+  ];
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
     _controller = VideoPlayerController.networkUrl(
-      Uri.parse(
-        "https://video-previews.elements.envatousercontent.com/9376b5da-eeec-4497-81ec-24974097b70c/watermarked_preview/watermarked_preview.mp4",
-      ),
+      videos[0],
       videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
     );
 
@@ -37,10 +40,14 @@ class _VideoScreenState extends State<VideoScreen> {
     _controller.setLooping(_looping);
     _controller.setVolume(1.0);
 
+    /* ValueListenableBuilder'e taşındı.
     _controller.addListener(() {
+      setState(() {});
+
       if (_controller.value.hasError)
         debugPrint("Video Error: ${_controller.value.errorDescription}");
     });
+     */
   }
 
   @override
@@ -87,11 +94,21 @@ class _VideoScreenState extends State<VideoScreen> {
             children: [
               AspectRatio(
                 aspectRatio: value.aspectRatio == 0
-                    ? 16/9
+                    ? 16 / 9
                     : value.aspectRatio,
                 child: VideoPlayer(_controller),
               ),
-              controlCard(value)
+              ValueListenableBuilder(
+                valueListenable: _controller,
+                builder: (context, VideoPlayerValue value, child) {
+                  if (value.hasError) {
+                    return Center(
+                      child: Text("Hata: ${value.errorDescription}"),
+                    );
+                  }
+                  return controlCard(value);
+                },
+              ),
             ],
           );
         },
@@ -104,107 +121,128 @@ class _VideoScreenState extends State<VideoScreen> {
       elevation: 4,
       margin: EdgeInsetsGeometry.symmetric(horizontal: 2, vertical: 2),
       color: Colors.blueAccent,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center ,
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          /// Oynat
-          IconButton(
-            iconSize: 22,
-            padding: EdgeInsets.all(3),
-            constraints: const BoxConstraints(),
-            onPressed: () {
-              setState(() {
-                value.isPlaying ? _controller.pause() : _controller.play();
-              });
-            },
-            icon: value.isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
-          ),
-
-          /// Geri Sar
-          IconButton(
-            iconSize: 22,
-            padding: EdgeInsets.all(3),
-            constraints: const BoxConstraints(),
-            onPressed: () {
-              final back = value.position - Duration(seconds: 10);
-              _controller.seekTo(
-                back > Duration(seconds: 0) ? back : value.position,
-              );
-            },
-            icon: Icon(Icons.replay_10),
-          ),
-
-          /// İleri Sar
-          IconButton(
-            iconSize: 22,
-            padding: EdgeInsets.all(3),
-            constraints: const BoxConstraints(),
-            onPressed: () {
-              final forward = value.position + Duration(seconds: 10);
-              _controller.seekTo(
-                forward < value.duration ? forward : value.duration,
-              );
-            },
-            icon: Icon(Icons.forward_10),
-          ),
-
-          /// Ses
-          IconButton(
-            iconSize: 22,
-            padding: EdgeInsets.all(3),
-            constraints: const BoxConstraints(),
-            onPressed: () async {
-              setState(() => _muted = !_muted);
-              await _controller.setVolume(_muted ? 0.0 : 1.0);
-            },
-            icon: _muted ? Icon(Icons.volume_off) : Icon(Icons.volume_up),
-          ),
-
-          Expanded(
-            child: Slider(
-              value: value.position.inMilliseconds.toDouble().clamp(
-                0,
-                value.duration.inMilliseconds.toDouble(),
+          Row(
+            children: [
+              SizedBox(width: 5),
+              Text(_format(value.position)),
+              Expanded(
+                child: SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    trackHeight: 3,
+                    thumbShape: const RoundSliderThumbShape(
+                      enabledThumbRadius: 6,
+                    ),
+                    overlayShape: const RoundSliderOverlayShape(
+                      overlayRadius: 12,
+                    ),
+                  ),
+                  child: Slider(
+                    value: value.position.inMilliseconds.toDouble().clamp(
+                      0,
+                      value.duration.inMilliseconds.toDouble(),
+                    ),
+                    min: 0,
+                    max: value.duration.inMilliseconds == 0
+                        ? 0
+                        : value.duration.inMilliseconds.toDouble(),
+                    onChanged: (ms) async {
+                      await _controller.seekTo(
+                        Duration(milliseconds: ms.toInt()),
+                      );
+                      setState(() {});
+                    },
+                  ),
+                ),
               ),
-              min: 0,
-              max: value.duration.inMilliseconds == 0
-                  ? 0
-                  : value.duration.inMilliseconds.toDouble(),
-              onChanged: (ms) async {
-                final pos = Duration(milliseconds: ms.toInt());
-                await _controller.seekTo(pos);
-                setState(() {});
-              },
-            ),
+              Text(_format(value.duration)),
+              SizedBox(width: 5),
+            ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              /// Oynat
+              IconButton(
+                iconSize: 22,
+                padding: EdgeInsets.all(3),
+                constraints: const BoxConstraints(),
+                onPressed: () {
+                  setState(() {
+                    value.isPlaying ? _controller.pause() : _controller.play();
+                  });
+                },
+                icon: value.isPlaying
+                    ? Icon(Icons.pause)
+                    : Icon(Icons.play_arrow),
+              ),
 
-          Text(_format(value.position)),
-          Text("/"),
-          Text(_format(value.duration)),
+              /// Geri Sar
+              IconButton(
+                iconSize: 22,
+                padding: EdgeInsets.all(3),
+                constraints: const BoxConstraints(),
+                onPressed: () {
+                  final back = value.position - Duration(seconds: 10);
+                  _controller.seekTo(
+                    back > Duration(seconds: 0) ? back : Duration.zero,
+                  );
+                },
+                icon: Icon(Icons.replay_10),
+              ),
 
-          /// Loop
-          IconButton(
-            iconSize: 22,
-            padding: EdgeInsets.all(3),
-            constraints: const BoxConstraints(),
-            onPressed: () {
-              setState(() => _looping = !_looping);
-              _controller.setLooping(_looping);
-            },
-            icon: _looping ? Icon(Icons.repeat_one) : Icon(Icons.repeat),
-            color: _looping ? Colors.red : Colors.white,
-          ),
+              /// İleri Sar
+              IconButton(
+                iconSize: 22,
+                padding: EdgeInsets.all(3),
+                constraints: const BoxConstraints(),
+                onPressed: () {
+                  final forward = value.position + Duration(seconds: 10);
+                  _controller.seekTo(
+                    forward < value.duration ? forward : value.duration,
+                  );
+                },
+                icon: Icon(Icons.forward_10),
+              ),
 
-          /// Full Ekran
-          IconButton(
-            iconSize: 22,
-            padding: EdgeInsets.all(3),
-            constraints: const BoxConstraints(),
-            onPressed: () {
-              setState(() => _fullscreen = !_fullscreen);
-            },
-            icon: Icon(Icons.fullscreen),
+              /// Ses
+              IconButton(
+                iconSize: 22,
+                padding: EdgeInsets.all(3),
+                constraints: const BoxConstraints(),
+                onPressed: () async {
+                  setState(() => _muted = !_muted);
+                  await _controller.setVolume(_muted ? 0.0 : 1.0);
+                },
+                icon: _muted ? Icon(Icons.volume_off) : Icon(Icons.volume_up),
+              ),
+
+              /// Loop
+              IconButton(
+                iconSize: 22,
+                padding: EdgeInsets.all(3),
+                constraints: const BoxConstraints(),
+                onPressed: () {
+                  setState(() => _looping = !_looping);
+                  _controller.setLooping(_looping);
+                },
+                icon: _looping ? Icon(Icons.repeat_one) : Icon(Icons.repeat),
+                color: _looping ? Colors.red : Colors.white,
+              ),
+
+              /// Full Ekran
+              IconButton(
+                iconSize: 22,
+                padding: EdgeInsets.all(3),
+                constraints: const BoxConstraints(),
+                onPressed: () {
+                  setState(() => _fullscreen = !_fullscreen);
+                },
+                icon: Icon(Icons.fullscreen),
+              ),
+            ],
           ),
         ],
       ),
